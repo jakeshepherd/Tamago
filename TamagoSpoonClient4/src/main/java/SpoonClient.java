3ff8bafe24a1943c748d7f3c58e185af831e359a
corradg <corradinigabriel0@gmail.com>
2019-03-01 11:32:59 +0000

    Improved documentation, added URL Generation Function, added get functions

--- TamagoSpoonClient4/src/main/java/SpoonClient.java
+++ TamagoSpoonClient4/src/main/java/SpoonClient.java
@@ -8,7 +8,32 @@ import org.json.JSONObject;
 
 public class SpoonClient {
     private String apiKey = "ac668d27e9mshb785cfffd22f03dp1f71e6jsn171dfa398c3b";
+    private String baseUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/";
+
+    /**
+     *  Generates a URL to be used for the Search Recipes By Ingredients Endpoint
+     * @param ingredients: single string of all ingredients split by comma and no whitespace (eg: "apples,flour,sugar")
+     * @param resultNum: number of results you want URL to return
+     * @param ranking: int value 1 or 2:
+     *               1: maximize used ingredients
+     *               2: minimize missing ingredients
+     * @return a functioning URL to perform API request with
+     */
+    public String URLGeneratorRBI(String ingredients, int resultNum, int ranking){
+        String ingURL = ingredients.replace(",", "%2C");
+        String ApiURL = baseUrl.concat("findByIngredients")
+                .concat(String.format("?number=%d", resultNum))
+                .concat(String.format("&ranking=%d", ranking))
+                .concat(String.format("&ingredients=%s", ingURL));
+        return ApiURL;
+    }
+
+    /**
+     * API Request functions below with test url
+     */
     String urlTest = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ingredients=apples%2Cflour%2Csugar";
+
+    // Requesting Function if Endpoint returns JSONArray
     public JSONArray getAsJSONArray(String Url, String Key){
         JSONArray json;
         try {
@@ -28,6 +53,7 @@ public class SpoonClient {
         return json;
     }
 
+    // Requesting function if Endpoint returns a JSONObject
     public JSONObject getAsJSONObject(String Url, String Key) {
         JSONObject json;
         try {
@@ -46,6 +72,7 @@ public class SpoonClient {
         }
         return json;
     }
+
     // functions below work off of getRecipesByIngredient Endpoint
     /**
      *
@@ -73,34 +100,72 @@ public class SpoonClient {
         return ID;
     }
 
+    //the following function obtains the JSONObject from the Get Recipe Information Endpoint
     public JSONObject getRecipeInfo(String Url, String Key, int index){
         String ID = getRecipeID(Url, Key, index);
         String InfoUrl = String.format("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/%s/information",ID);
         JSONObject recipeJson = getAsJSONObject(InfoUrl, apiKey);
         return recipeJson;
     }
+
     // the following functions use the GetRecipeInformation endpoint
+    /**
+     *
+     * @param Url: API Request URL
+     * @param Key: API Key
+     * @param index: index of the request result (which recipe) to get the recipe info from
+     * @return Instructions: String containing recipe instructions
+     */
     public String getRecipeInstructions(String Url, String Key, int index){
         JSONObject json = getRecipeInfo(Url, Key, index);
         String Instructions = json.get("instructions").toString();
         return Instructions;
     }
 
-    public String getIngredientName(String Url, String Key, int index, int rIndex){
+    /**
+     *
+     * @param Url: API Request URL
+     * @param Key: API Key
+     * @param index: index of the request result (which recipe) to get the recipe info from
+     * @return RecipeTitle: String of the Recipe's name given by the API
+     */
+    public String getRecipeTitle(String Url, String Key, int index){
         JSONObject json = getRecipeInfo(Url, Key, index);
-        String ingredientName = json.getJSONArray("extendedIngredients").getJSONObject(rIndex).get("name").toString();
-        return ingredientName;
-    }
-    public String getIngredientInfo(String Url, String Key, int index, int rIndex){
-        JSONObject json = getRecipeInfo(Url, Key, index);
-        String ingredientName = json.getJSONArray("extendedIngredients").getJSONObject(rIndex).get("originalString").toString();
-        return ingredientName;
-    }
-    public String getIngredientInfos(JSONArray IngredientsList, int rIndex){
-        String ingredientName = IngredientsList.getJSONObject(rIndex).get("originalString").toString();
-        return ingredientName;
+        String RecipeTitle = json.get("title").toString();
+        return RecipeTitle;
     }
 
+    /**
+     *
+     * @param Url: API Request URL
+     * @param Key: API Key
+     * @param index: index of the request result (which recipe) to get the recipe info from
+     * @return RecipeMins: String of time needed to make recipe
+     */
+    public String getRecipeMins(String Url, String Key, int index){
+        JSONObject json = getRecipeInfo(Url, Key, index);
+        String RecipeMins = json.get("readyInMinutes").toString();
+        return RecipeMins;
+    }
+
+    /**
+     *
+     * @param IngredientsList: JSONArray of Ingredients from the JSON Response of the API
+     * @param rIndex: index of which ingredient of the recipe to get name of
+     * @return Ingredient: String of ingredient name and quantity needed for recipe
+     */
+    public String getIngredientInfos(JSONArray IngredientsList, int rIndex){
+        String ingredient = IngredientsList.getJSONObject(rIndex).get("originalString").toString();
+        return ingredient;
+    }
+
+    /**
+     *
+     * @param Url: API Request URL
+     * @param Key: API Key
+     * @param index: index of the request result (which recipe) to get the recipe info from
+     * @return ingredientsList: String array that contains all the ingredients for given recipe
+     */
     public String[] getIngredientList(String Url, String Key, int index) {
         JSONArray recipeInfo = getRecipeInfo(Url, Key, index).getJSONArray("extendedIngredients");
         int listLength = recipeInfo.length();
@@ -110,5 +175,32 @@ public class SpoonClient {
             ingredientsList[i] = ingredient;
         }
         return ingredientsList;
+    }
+
+    /**
+     * To get values from this function use returned.get("parameter").toString();
+     * @param Url API Request URL
+     * @param Key: API Key
+     * @param index: index of the request result (which recipe) to get the recipe info from
+     * @return a filtered version of the JSON Object returned by the Endpoint excluding ingredients data
+     */
+    public JSONObject getAllRecipeData(String Url, String Key, int index){
+        JSONObject recipeInfo = getRecipeInfo(Url, Key, index);
+        JSONObject recipeArr = new JSONObject();
+        //Vegetarian
+        recipeArr.put("vegetarian", recipeInfo.get("vegetarian").toString());
+        //Vegan
+        recipeArr.put("vegan", recipeInfo.get("vegan").toString());
+        //Gluten-free
+        recipeArr.put("glutenFree", recipeInfo.get("glutenFree").toString());
+        //title
+        recipeArr.put("name", recipeInfo.get("title").toString());
+        //mins
+        recipeArr.put("readyMins", recipeInfo.get("readyInMinutes").toString());
+        //imageURL
+        recipeArr.put("imageURL", recipeInfo.get("image").toString());
+        //Instructions
+        recipeArr.put("instructions", recipeInfo.get("instructions").toString());
+        return recipeArr;
     }
 }
