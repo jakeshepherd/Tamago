f1ed2b1271e2e24e1f6c619a204199bae3ab4f9b
jakeshepherd <36624642+jakeshepherd@users.noreply.github.com>
2019-03-07 15:24:13 +0000

    Update MainActivity.java

--- app/src/main/java/com/example/jakeshepherd/tamago/MainActivity.java
+++ app/src/main/java/com/example/jakeshepherd/tamago/MainActivity.java
@@ -14,22 +14,6 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    /* Button insert;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        insert = (Button) findViewById(R.id.insertButton);
-
-        insert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Button Test", Toast.LENGTH_LONG).show();//display the text of button1
-            }
-        });
-    } */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         ShoppingList.getInstance();
