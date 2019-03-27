9c04396b5f0dfe318c9bda9d3dac331fc02c519b
corradg <corradinigabriel0@gmail.com>
2019-03-27 14:44:16 +0000

    recipeDB class added with test usage in APILink

--- app/src/main/java/com/example/jakeshepherd/tamago/APILink.java
+++ app/src/main/java/com/example/jakeshepherd/tamago/APILink.java
@@ -1,26 +1,17 @@
 package com.example.jakeshepherd.tamago;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import org.json.JSONObject;
-import org.json.JSONException;
-import com.tamago.spoonclient4.SpoonClient;
 
 public class APILink extends AppCompatActivity {
 
     private String apiKey = "ac668d27e9mshb785cfffd22f03dp1f71e6jsn171dfa398c3b";
     private String baseUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/";
-    private SpoonClient sc = new SpoonClient();
+    private TamagoRecipeDB RDB = new TamagoRecipeDB(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,23 +24,22 @@ public class APILink extends AppCompatActivity {
 
         test.setOnClickListener(new View.OnClickListener() {
             @Override
+            /**
+             * at the moment set to return the instructions on first search result that uses eggs
+             */
             public void onClick(View view) {
                 /* For now will just assume that it is input in the correct form of a string
                  of ingredients split by comma and no whitespace (eg: "apples,flour,sugar") */
-                String ingredients = ((EditText)findViewById(R.id.foodItem)).getText().toString();
+                String ingredient = ((EditText)findViewById(R.id.foodItem)).getText().toString();
                 /* Generates URL, again just for now we'll get one recipe back but can change
                 resultNum to get more once all of this is working */
-                String URL = sc.URLGeneratorRBI(ingredients, 1, 1);
-                try {
-                    JSONObject returnedRecipe = sc.getAllRecipeData(URL, apiKey, 1);
-                    /* For now I'm just getting the recipe instructions but there are other parameters
-                    to get back once it's all fully working */
-                    String returnedInstructions = returnedRecipe.get("instructions").toString();
-                    TextView textView = (TextView) findViewById(R.id.testResults);
-                    textView.setText(returnedInstructions);
-                } catch(JSONException e) {
+                String searchQuery = RDB.getRecipeIDwithIng(ingredient);
+                String returnedInstructions = RDB.getInstructions(searchQuery);
+                /* For now I'm just getting the recipe instructions but there are other parameters
+                to get back once it's all fully working */
+                TextView textView = (TextView) findViewById(R.id.testResults);
+                textView.setText(returnedInstructions);
 
-                }
             }
         });
     }
